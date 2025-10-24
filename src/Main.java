15c914a949b6bce42e4391ced3258370c623fda1
selmapelu <selmakadirpelu@gmail.com>
2025-10-24 16:57:34 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,44 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Git!");
-        System.out.println("This is my second commit!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("=== Kalkulator Sederhana ===");
+        System.out.print("Masukkan angka pertama: ");
+        double angka1 = input.nextDouble();
+
+        System.out.print("Masukkan angka kedua: ");
+        double angka2 = input.nextDouble();
+
+        System.out.println("Pilih operasi (+, -, *, /): ");
+        char operasi = input.next().charAt(0);
+
+        double hasil;
+
+        switch (operasi) {
+            case '+':
+                hasil = angka1 + angka2;
+                break;
+            case '-':
+                hasil = angka1 - angka2;
+                break;
+            case '*':
+                hasil = angka1 * angka2;
+                break;
+            case '/':
+                if (angka2 != 0) {
+                    hasil = angka1 / angka2;
+                } else {
+                    System.out.println("Error: Tidak bisa dibagi dengan nol!");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Operasi tidak valid!");
+                return;
+        }
+
+        System.out.println("Hasil: " + hasil);
     }
 }
